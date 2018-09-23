e8cf2e56df64eed23e4f3f709d7bcaa2f82116f0
keinstn <kei.sj.nstn@gmail.com>
2018-09-24 08:30:50 +0900

    Modified pyenv settings of fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,11 @@ source ~/.config/fish/aliases.fish
 
 fish_vi_key_bindings
 
-set -g simple_ass_prompt_greeting 
+set -g simple_ass_prompt_greeting
 
 set -g FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
 source $HOME/.cargo/env
 
-eval (direnv hook fish)
-. (pyenv init - | psub)
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
