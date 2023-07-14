469816f9ffb797e083e8e7ba59345f582f680291
keinstn <kei.sj.nstn@gmail.com>
2023-07-15 07:27:52 +0900

    [nvim] Add rustfmt for rust formatter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -243,6 +243,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.black,
 		null_ls.builtins.formatting.dart_format,
 		null_ls.builtins.formatting.phpcsfixer,
+		null_ls.builtins.formatting.rustfmt,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.gofumpt,
 		null_ls.builtins.formatting.goimports,
