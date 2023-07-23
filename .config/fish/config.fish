50b9e249140e05e8fa8f48adf8e95866d270fe6b
keinstn <kei.sj.nstn@gmail.com>
2023-07-23 11:48:26 +0900

    [fish] Added flutter path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,3 +34,6 @@ starship init fish | source
 # Wasmer
 export WASMER_DIR="$HOME/.wasmer"
 [ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
+
+# flutter
+set -x PATH "$HOME/.pub-cache/bin" $PATH
