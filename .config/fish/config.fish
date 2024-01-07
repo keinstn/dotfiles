3192d740a1ea75e608e1d8b25f9da80a10b3d654
keinstn <kei.sj.nstn@gmail.com>
2024-01-07 20:17:23 +0900

    [fish][phpbrew] Enabled .phpbrewrc file searching

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -31,4 +31,5 @@ set -x WEZTERM_CONFIG_FILE "$HOME/.config/wezterm/wezterm.lua"
 set -Ua fish_user_paths "$HOME/.rye/shims"
 
 # phpbrew
+set -x PHPBREW_RC_ENABLE 1
 source $HOME/.phpbrew/phpbrew.fish
