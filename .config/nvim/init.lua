f1eab4a9ea7c1a5cebe6ddd5069120e9070ee3c0
keinstn <kei.sj.nstn@gmail.com>
2024-09-15 07:35:47 +0900

    [nvim] Added ruff to python formatter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -200,7 +200,7 @@ require("lazy").setup({
         require("conform").setup({
           formatters_by_ft = {
             lua = { "stylua" },
-            python = { "isort", "black" },
+            python = { "isort", "ruff", "black" },
             rust = { "rustfmt" },
             javascript = { "prettierd", "prettier", stop_after_first = true },
           },
