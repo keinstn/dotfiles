d4b1f06376559801227ada65265fc7dbf905fa9a
keinstn <kei.sj.nstn@gmail.com>
2024-12-09 20:56:58 +0900

    [nvim] Added avante.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -251,6 +251,55 @@ require("lazy").setup({
         require("transparent").clear_prefix("NeoTree")
       end,
     },
+    {
+      "yetone/avante.nvim",
+      event = "VeryLazy",
+      lazy = false,
+      version = false,
+      opts = {
+        provider = "copilot",
+      },
+      build = "make",
+      dependencies = {
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "hrsh7th/nvim-cmp",
+        "nvim-tree/nvim-web-devicons",
+        {
+          "zbirenbaum/copilot.lua",
+          cmd = "Copilot",
+          event = "InsertEnter",
+          config = function()
+            require("copilot").setup({})
+          end,
+        },
+        {
+          -- support for image pasting
+          "HakonHarnes/img-clip.nvim",
+          event = "VeryLazy",
+          opts = {
+            -- recommended settings
+            default = {
+              embed_image_as_base64 = false,
+              prompt_for_file_name = false,
+              drag_and_drop = {
+                insert_mode = true,
+              },
+              -- required for Windows users
+              use_absolute_path = true,
+            },
+          },
+        },
+        {
+          "MeanderingProgrammer/render-markdown.nvim",
+          opts = {
+            file_types = { "markdown", "Avante" },
+          },
+          ft = { "markdown", "Avante" },
+        },
+      },
+    },
   },
   install = { colorscheme = { "habamax" } },
   checker = { enabled = true },
