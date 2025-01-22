c1197fb645ed0936bfb9753e029131b4d3a4c136
keinstn <kei.sj.nstn@gmail.com>
2025-01-22 16:27:39 +0900

    [nvim] Added Molten

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -298,6 +298,28 @@ require("lazy").setup({
         },
       },
     },
+    {
+      "benlubas/molten-nvim",
+      version = "^1.0.0",
+      dependencies = { "3rd/image.nvim" },
+      build = ":UpdateRemotePlugins",
+      init = function()
+        g.molten_image_provider = "image.nvim"
+        g.molten_output_win_max_height = 20
+      end,
+    },
+    {
+      "3rd/image.nvim",
+      opts = {
+        backend = "kitty",
+        max_width = 100,
+        max_height = 12,
+        max_height_window_percentage = math.huge,
+        max_width_window_percentage = math.huge,
+        window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      },
+    },
   },
   checker = { enabled = true },
 })
