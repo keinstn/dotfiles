cac5cbf66d6e6f38e79216f25b9d05e589c7a422
keinstn <kei.sj.nstn@gmail.com>
2025-05-12 20:49:53 +0900

    [wezterm] Enable split pane key bindings for macos

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -23,59 +23,59 @@ config.window_background_opacity = is_unix and 0.9 or 0.7
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 14.0
 
-if not is_unix then
-  -- Emulate tmux key bingings
-  config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 1000 }
-  config.keys = {
-    {
-      key = "v",
-      mods = "LEADER",
-      action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-    },
-    {
-      key = "s",
-      mods = "LEADER",
-      action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-    },
-    {
-      key = "d",
-      mods = "CTRL",
-      action = wezterm.action.CloseCurrentPane({ confirm = true }),
-    },
-    {
-      key = "h",
-      mods = "CTRL",
-      action = act.ActivatePaneDirection("Left"),
-    },
-    {
-      key = "l",
-      mods = "CTRL",
-      action = act.ActivatePaneDirection("Right"),
-    },
-    {
-      key = "k",
-      mods = "CTRL",
-      action = act.ActivatePaneDirection("Up"),
-    },
-    {
-      key = "j",
-      mods = "CTRL",
-      action = act.ActivatePaneDirection("Down"),
-    },
-    {
-      key = "c",
-      mods = "LEADER",
-      action = act.SpawnTab("CurrentPaneDomain"),
-    },
-    {
-      key = "x",
-      mods = "LEADER",
-      action = act.PaneSelect({
-        mode = "SwapWithActive",
-      }),
-    },
-  }
+-- Emulate tmux key bingings
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+  {
+    key = "v",
+    mods = "LEADER",
+    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "s",
+    mods = "LEADER",
+    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "d",
+    mods = "CTRL",
+    action = wezterm.action.CloseCurrentPane({ confirm = true }),
+  },
+  {
+    key = "h",
+    mods = "CTRL",
+    action = act.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "l",
+    mods = "CTRL",
+    action = act.ActivatePaneDirection("Right"),
+  },
+  {
+    key = "k",
+    mods = "CTRL",
+    action = act.ActivatePaneDirection("Up"),
+  },
+  {
+    key = "j",
+    mods = "CTRL",
+    action = act.ActivatePaneDirection("Down"),
+  },
+  {
+    key = "c",
+    mods = "LEADER",
+    action = act.SpawnTab("CurrentPaneDomain"),
+  },
+  {
+    key = "x",
+    mods = "LEADER",
+    action = act.PaneSelect({
+      mode = "SwapWithActive",
+    }),
+  },
+}
 
+if not is_unix then
   config.default_prog = { "nu.exe" }
 else
   config.default_prog = { "/opt/homebrew/bin/nu" }
