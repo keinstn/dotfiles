910d4c5db2606085674316cc19a97251c45dbdda
keinstn <kei.sj.nstn@gmail.com>
2025-05-26 13:10:55 +0900

    [nvim] Added mcphub plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -233,6 +233,20 @@ require("lazy").setup({
         "nvim-treesitter/nvim-treesitter",
         "github/copilot.vim",
       },
+      config = function()
+        require("codecompanion").setup({
+          extensions = {
+            mcphub = {
+              callback = "mcphub.extensions.codecompanion",
+              opts = {
+                show_result_in_chat = true, -- Show mcp tool results in chat
+                make_vars = true,           -- Convert resources to #variables
+                make_slash_commands = true, -- Add prompts as /slash commands
+              },
+            },
+          },
+        })
+      end,
     },
     {
       "pwntester/octo.nvim",
@@ -244,6 +258,13 @@ require("lazy").setup({
       config = function()
         require("octo").setup()
       end,
+    },
+    {
+      "ravitemer/mcphub.nvim",
+      build = "npm install -g mcp-hub@latest",
+      config = function()
+        require("mcphub").setup()
+      end
     },
     { "segeljakt/vim-silicon" },
     {
